8b519b4c7a4a7ebbc45c8887f20a0bd811b2acf2
Aom <radtanaporn.kn@gmail.com>
2018-09-17 16:51:04 +0700

    install

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-  host     : 'https://www.db4free.net',
+  host     : 'www.db4free.net',
   user     : 's140390',
   password : 'abc123**',
   database : 'db140390'
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.get('/home', function (req, res){
     connection.query('SELECT * FROM student', function (error, results, fields) {
         console.log('DATA'+results);
-        res.render('pages/home',{home : data})
+        res.render('pages/home',{home : results})
       });
 });
 
